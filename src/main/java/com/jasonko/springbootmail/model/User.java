f06a84d024487b67d3452f98936a55944272a1eb
jasongryo <f2675805@gmail.com>
2025-03-16 14:36:07 +0800

    帳號功能-檢查註冊email

--- src/main/java/com/jasonko/springbootmail/model/User.java
+++ src/main/java/com/jasonko/springbootmail/model/User.java
@@ -1,10 +1,14 @@
 package com.jasonko.springbootmail.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import java.util.Date;
 
 public class User {
     private Integer userId;
     private String email;
+
+    @JsonIgnore
     private String password;
     private Date createDate;
     private Date lastModifyDate;
--- src/main/java/com/jasonko/springbootmail/service/impl/UserServiceImpl.java
+++ src/main/java/com/jasonko/springbootmail/service/impl/UserServiceImpl.java
@@ -4,11 +4,17 @@ import com.jasonko.springbootmail.dao.UserDao;
 import com.jasonko.springbootmail.dto.UserRegisterRequest;
 import com.jasonko.springbootmail.model.User;
 import com.jasonko.springbootmail.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
 
 @Component
 public class UserServiceImpl implements UserService {
+
+    private final static Logger log = LoggerFactory.getLogger(UserServiceImpl.class);
 
     @Autowired
     private UserDao userDao;
@@ -20,6 +26,14 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public Integer register(UserRegisterRequest userRegisterRequest) {
+        //  檢查註冊email
+        User user = userDao.getUserByEmail(userRegisterRequest.getEmail());
+
+        if (user != null) {
+            log.warn("該email {} 已經被註冊", userRegisterRequest.getEmail());
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
         return userDao.createUser(userRegisterRequest);
     }
 }
